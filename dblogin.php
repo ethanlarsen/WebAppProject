a5478bca5a1219ebd3d5c2540024519fa3349a1e
CBaddley <62369843+CBaddley@users.noreply.github.com>
2020-04-19 20:45:48 -0600

    Update dblogin.php
    for mac

--- dblogin.php
+++ dblogin.php
@@ -1,7 +1,7 @@
 <?php
-$hn = "localhost:3306"; //host name
+$hn = "localhost"; //host name
 $db = "LibraryDB"; //database
 $un = "root"; //username
-$pw = ""; //password -- I'm on a Mac so it is also root
+$pw = "root"; //password -- I'm on a Mac so it is also root
 
 ?>
